49b919ad14671e186c926a48580a72b55e24007d
amcodin <amichel.tech@gmail.com>
2020-06-09 17:37:29 +1000

    code review

---
+++ controllers/register.js
@@ -0,0 +1,33 @@
+const handleRegister = (req, res, db, bcrypt, saltRounds) => {
+  const { email, name, password } = req.body;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  db.transaction((trx) => {
+    //create a transaction when we have to do more than two things at once when modifying database items.
+    trx //use trx, instead of db, to do the operations.
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]); //respond it with JSON.
+          });
+      })
+      .then(trx.commit) //for the above to get added, we need to commit.
+      .catch(trx.rollback); //if anything fails, we roll back the changes.
+  }).catch((err) => res.status(400).json("unable to join"));
+};
+
+module.exports = {
+  handleRegister: handleRegister,
+};
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const knex = require("knex");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const image = require("./controllers/image");
+const profile = require("./controllers/profile");
+
 const db = knex({
   client: "pg",
   connection: {
@@ -24,84 +29,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/profile/:id", (req, res) => {
-  const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({
-      id: id,
-    })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("error getting user");
-      }
-    })
-    .catch((err) => res.status(400).json("error getting user"));
+  profile.handleProfile(req, res, db);
 });
 
 app.put("/image", (req, res) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => {
-      res.json(entries[0]);
-    })
-    .catch((err) => res.status(400).json("unable to get entries"));
+  image.handleImage(req, res, db);
 });
 
 app.post("/signin", (req, res) => {
-  db.select("email", "hash")
-    .from("login")
-    .where("email", "=", req.body.email)
-    .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-      if (isValid) {
-        return db
-          .select("*")
-          .from("users")
-          .where("email", "=", req.body.email)
-          .then((user) => {
-            res.json(user[0]);
-          })
-          .catch((err) => res.status(400).json("unable to get user"));
-      } else {
-        res.status(400).json("wrong credentials");
-      }
-    })
-    .catch((err) => res.status(400).json("wrong credentials"));
+  signin.handleSignIn(req, res, db, bcrypt);
 });
 
 app.post("/register", (req, res) => {
-  const { email, name, password } = req.body;
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(password, salt);
-  db.transaction((trx) => {
-    //create a transaction when we have to do more than two things at once when modifying database items.
-    trx //use trx, instead of db, to do the operations.
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            email: loginEmail[0],
-            name: name,
-            joined: new Date(),
-          })
-          .then((user) => {
-            res.json(user[0]); //respond it with JSON.
-          });
-      })
-      .then(trx.commit) //for the above to get added, we need to commit.
-      .catch(trx.rollback); //if anything fails, we roll back the changes.
-  }).catch((err) => res.status(400).json("unable to join"));
+  register.handleRegister(req, res, db, bcrypt, saltRounds);
 });
 
 app.listen(3000, () => {
