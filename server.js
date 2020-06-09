958a44267ba21296f9858cf4cb31d08753db4195
amcodin <amichel.tech@gmail.com>
2020-06-09 15:46:47 +1000

    register, bcrypt and transaction trx

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const knex = require("knex");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const db = knex({
   client: "pg",
   connection: {
     host: "127.0.0.1",
     user: "postgres",
-    password: "PASSWORD",
+    password: "1592",
     database: "smartbrain",
   },
 });
@@ -91,25 +93,31 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  db("users")
-    .returning("*")
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("unable to join"));
-  // database.users.push({
-  //   id: "125",
-  //   name: name,
-  //   email: email,
-  //   entries: 0,
-  //   joined: new Date(),
-  // });
-  // res.json(database.users[database.users.length - 1]);
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to join"));
 });
 
 app.listen(3000, () => {
